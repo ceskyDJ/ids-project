ef8acd4cf84bbd746b3f71932b8e7d0efd721783
Martin Havlík <omnitex.git@gmail.com>
2022-04-29 14:27:58 +0200

    Attempt to do some indexes to improve execution plan

--- 04_final/final.sql
+++ 04_final/final.sql
@@ -633,6 +633,8 @@ INSERT INTO question_assessments (exam_elaboration_id, question_number, lecturer
     VALUES (162, 4, 220300, 14, CURRENT_TIMESTAMP);
 INSERT INTO question_assessments (exam_elaboration_id, question_number, lecturer_id, awarded_points, time_of_assessments)
     VALUES (162, 5, 220300, 15, CURRENT_TIMESTAMP);
+
+COMMIT;
 ------------------------------------------------------------------------------------------------------------------ VIEWS
 
 
@@ -778,6 +780,35 @@ BEGIN
                   question_number = question.question_number;
     END LOOP;
 END;
+
+----------------------------------------------------------------------------------------------------------- INDEXES
+-- TODO doing this the obvious and dumb way...
+DROP INDEX ee_exam_id_IX;
+CREATE INDEX ee_exam_id_IX ON exam_elaborations (exam_id);
+-- improves 6: TABLE ACCESS FULL -> TABLE ACCESS BY INDEX ROWID BATCHED, INDEX RANGE SCAN
+
+DROP INDEX qa_exam_elaboration_id_IX;
+CREATE INDEX qa_exam_elaboration_id_IX ON question_assessments (exam_elaboration_id DESC);
+-- does A LOT OF STUFF to 9: may not be better because INDEX FULL SCAN and the other stuff...
+
+COMMIT;
+
+EXPLAIN PLAN FOR
+WITH summary_results AS (
+    SELECT exam_date_number, student_id, SUM(qa.awarded_points) students_summary_points
+        FROM enrolled_students st
+        JOIN registered_exam_dates rd USING (student_id, academic_year)
+        JOIN exam_dates ed USING (exam_id, exam_date_number)
+        JOIN exam_elaborations el USING (student_id, academic_year, exam_id, exam_date_number)
+        JOIN question_assessments qa USING (exam_elaboration_id)
+        WHERE exam_id = 62
+        GROUP BY exam_date_number, student_id
+)
+SELECT exam_date_number, AVG(students_summary_points) points_average
+    FROM summary_results
+    GROUP BY exam_date_number;
+
+SELECT * FROM table(DBMS_XPLAN.DISPLAY());
 
 ----------------------------------------------------------------------------------------------------------- TEST INSERTS
 -- Check generating sequence for exam_date_number (bi_tg_exam_dates_dk)
